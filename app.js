df904e5d08726d36b749607f663746b6411d8f44
Charlito Mecarez Jr <70713746+developedbycharkun@users.noreply.github.com>
2022-09-19 19:29:31 +0800

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+const scoring = document.querySelector('.scoring');
+const userScore = document.querySelector('.user-score');
+const aiScore = document.querySelector('.ai-result');
+const aiResult = document.querySelector('.ai-result');
+const userResult = document.querySelector('.user-result');
+const rock = document.querySelector('.rock ');
+const paper = document.querySelector('.paper');
+const scissor = document.querySelector('.scissor');
